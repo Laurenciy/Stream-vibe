048d4fa72d63f30e951d4398a91bd7ac5b9ea028
Laurenciy <psorin675@gmail.com>
2025-06-23 01:45:22 +0300

    third commit
    Took 2 hours 33 minutes

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,31 @@
+import './Logo.scss'
+import classNames from 'classnames'
+
+const Logo = (props) => {
+  const {
+    className,
+    loading = 'lazy'
+  } = props
+const title = 'Home'
+
+  return (
+    <a
+    className="logo"
+    href="/"
+    title={title}
+    aria-label={title}
+    >
+      <img
+        src="/logo.svg"
+        alt=""
+        className={classNames(className, 'logo')}
+        width={199}
+        height={60}
+        loading={loading}
+        draggable="false"
+      />
+    </a>
+  )
+}
+
+export default Logo
