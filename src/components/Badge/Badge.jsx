c75afcb180a3db386ccd080c260710ab464c6aa7
Laurenciy <psorin675@gmail.com>
2025-07-16 01:52:02 +0300

    commit
    Took 1 hour 49 minutes

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,12 +1,16 @@
 import './Badge.scss'
 import classNames from 'classnames'
+import Icon from "@/components/Icon";
 
 const Badge = (props) => {
   const {
     className,
     mode = '',
     isBig = false,
-    children
+    children,
+    iconName,
+    hasFillIcon,
+    iconAriaLabel,
   } = props
 
   return (
@@ -16,7 +20,17 @@ const Badge = (props) => {
         'badge--big': isBig,
       })}
     >
-      {children}
+      {iconName && (
+        <Icon
+          className="badge__icon"
+          name={iconName}
+          hasFill={hasFillIcon}
+          ariaLabel={iconAriaLabel}
+        />
+      )}
+      <span>
+        {children}
+      </span>
     </div>
   )
 }
